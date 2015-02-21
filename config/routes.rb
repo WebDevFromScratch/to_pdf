36d130aaa382207cdee9ca6bd1cbfc1623fdfac8
Piotr Klosinski <mybettingstuff@gmail.com>
2015-02-21 13:36:34 +0100

    initial commit and set up

---
+++ config/routes.rb
@@ -0,0 +1,13 @@
+Rails.application.routes.draw do
+  devise_for :accounts
+
+  devise_scope :account do
+    authenticated :account do
+      root to: 'home#index', as: :authenticated_root
+    end
+
+    unauthenticated do
+      root to: 'devise/sessions#new', as: :unauthenticated_root
+    end
+  end
+end
